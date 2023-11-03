fc973291d922fb65407ad18cb85f141b7a9eacaa
Juan Iriart <jeiriart@gmail.com>
2023-11-03 10:16:31 -0300

    added react router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-const root = createRoot(document.getElementById("my-react-app"));
-root.render(<App />);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("my-react-app")
+);
