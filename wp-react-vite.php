5f3d8fec5a6bda94dc59ea0a9f809058651cdcd3
Juan Iriart <jeiriart@gmail.com>
2023-11-03 11:57:56 -0300

    added manifest.json to head

--- wp-react-vite.php
+++ wp-react-vite.php
@@ -10,6 +10,9 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('react-script', plugin_dir_url(__FILE__) . 'dist/assets/main.js', [], '1.0.1', true);
 });
 
+add_action('wp_head', function () {
+    echo '<link rel="manifest" href="' . plugin_dir_url(__FILE__) . 'dist/manifest.json" />';
+});
 
 add_filter('script_loader_tag', 'add_type_attribute', 10, 3);
 function add_type_attribute($tag, $handle, $src)
